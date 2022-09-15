80b2c5c56aa0dff4b910cd2e007cb3e7f97bc085
noormesslmani <noor.messlmani@gmail.com>
2022-09-15 21:33:44 +0300

    added logout Modal

--- twitter frontend/home.js
+++ twitter frontend/home.js
@@ -6,7 +6,26 @@ const themes=document.getElementById('themes')
 const more=document.getElementById('more')
 const tweetContents=document.getElementById('tweet-contents')
 const submitTweet=document.getElementById('submit-tweet')
-
+const body=document.getElementById('body')
+const modal = document.getElementById("myModal");
+const logOut = document.getElementById("logout-btn");
+const cancel = document.getElementById("cancel-btn");
 profile.addEventListener('click',()=>{
     window.location.replace('profile.html')
 })
+themes.addEventListener('click',()=>{
+    body.classList.toggle("black-back")
+})
+
+more.onclick = function() {
+    modal.style.display = "block";
+}
+cancel.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
--- twitter frontend/profile.js
+++ twitter frontend/profile.js
@@ -4,10 +4,29 @@ const twitter=document.getElementById('twitter')
 const profile=document.getElementById('profile')
 const themes=document.getElementById('themes')
 const more=document.getElementById('more')
-
+const body=document.getElementById('body')
+const modal = document.getElementById("myModal")
+const logOut = document.getElementById("logout-btn")
+const cancel = document.getElementById("cancel-btn")
 home.addEventListener('click',()=>{
     window.location.replace('./home.html')
 })
 twitter.addEventListener('click',()=>{
     window.location.replace('home.html')
 })
+themes.addEventListener('click',()=>{
+    body.classList.toggle("black-back")
+})
+
+more.onclick = function() {
+    modal.style.display = "block";
+}
+cancel.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
