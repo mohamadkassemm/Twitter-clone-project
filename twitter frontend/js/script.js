7d16af08badc4a6843cf0f658809f68acec3542a
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-15 13:51:09 +0300

    adding link between pages

--- twitter frontend/js/script.js
+++ twitter frontend/js/script.js
@@ -1,7 +1,10 @@
 const signin =document.getElementById("signin")
 const signup =document.getElementById("signup")
+const nextSignin =document.getElementById("nxt-signin")
+const nextSignup =document.getElementById("nxt-signup")
 const signinModal=document.getElementById("signin-modal")
 const signupModal=document.getElementById("signup-modal")
+const passPage=document.getElementById("passPage")
 
 // after clicking on signin the modal appears
 signin.addEventListener("click",()=>{
@@ -11,6 +14,12 @@ signin.addEventListener("click",()=>{
 signup.addEventListener("click",()=>{
     signupModal.style.display="flex"
 })
+// after clicking it shows the pass modal
+nextSignup.onclick= function (event){
+    event.preventDefault();
+    signupModal.style.display="none"
+    passPage.style.display="flex"
+} 
 // closing the modal whenever we click outside the border of the modals
 window.onclick = function(event) {
 
@@ -25,5 +34,14 @@ window.onclick = function(event) {
         signupModal.style.display = "none";
  
      }
+
+     if(event.target == passPage){
+        passPage.style.display= "none";
+     }
  
  }
+//  moves us to home page
+ nextSignin.onclick= function (event){
+    event.preventDefault();
+    window.location.replace('home.html')
+} 
