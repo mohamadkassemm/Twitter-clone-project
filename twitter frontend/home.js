70b78286c2a9cbb1823707d909e6250ec5c1d0cd
noormesslmani <noor.messlmani@gmail.com>
2022-09-17 23:30:01 +0300

    fetched for users with the same entered username upon ckicking on search icon

--- twitter frontend/home.js
+++ twitter frontend/home.js
@@ -1,4 +1,5 @@
 const search=document.getElementById('search')
+const searchIcon= document.getElementById('search-icon')
 const home=document.getElementById('home')
 const twitter=document.getElementById('twitter')
 const profile=document.getElementById('profile')
@@ -8,8 +9,9 @@ const tweetContents=document.getElementById('tweet-contents')
 const submitTweet=document.getElementById('submit-tweet')
 const body=document.getElementById('body')
 const modal = document.getElementById("myModal");
-const logOut = document.getElementById("logout-btn");
-const cancel = document.getElementById("cancel-btn");
+const logOut = document.getElementById("logout-btn")
+const cancel = document.getElementById("cancel-btn")
+const searchOutput = document.getElementById('search-output')
 profile.addEventListener('click',()=>{
     window.location.replace('profile.html')
 })
@@ -17,7 +19,7 @@ themes.addEventListener('click',()=>{
     body.classList.toggle("black-back")
     
 })
-
+console.log(window.localStorage)
 more.onclick = function() {
     modal.style.display = "block";
 }
@@ -29,4 +31,16 @@ window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
+}
+
+searchIcon.onclick= ()=>{
+    let userName=search.value
+    let url='http://localhost/twitter-testing/searchusers.php/?users_name='+userName
+    console.log(url)
+    fetch(url)
+    .then(Response => Response.json())
+    .then(data => {
+        localStorage.setItem('data',JSON.stringify(data))
+        window.location.replace('search.html')
+    })  
 }
