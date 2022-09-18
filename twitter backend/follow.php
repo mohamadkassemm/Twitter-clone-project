b3550d752440daec33cd8dc8c6a803c17be325ad
noormesslmani <noor.messlmani@gmail.com>
2022-09-18 12:40:14 +0300

    updated follow api to allow to handle unfollow case

--- twitter backend/follow.php
+++ twitter backend/follow.php
@@ -7,9 +7,27 @@ include("connection.php");
 
 $follower_id = $_POST["follower_id"];
 $followed_id = $_POST["followed_id"];
-$query = $mysqli->prepare("INSERT INTO follow_relations(follower_id,followed_id) VALUE (?,?)");
-$query->bind_param("ii", $follower_id, $followed_id);
+
+$query = $mysqli->prepare("SELECT follower_id, followed_id FROM follow_relations where follower_id='".$follower_id."' and followed_id='".$followed_id."' ");
 $query->execute();
+$array = $query->get_result();
+
+$response = '';
+
+while($a = $array->fetch_assoc()){  
+    $response= $a;
+}
+$response= json_encode($response);
+if ($response==''){
+    $query = $mysqli->prepare("INSERT INTO follow_relations(follower_id,followed_id) VALUE (?,?)");
+    $query->bind_param("ii", $follower_id, $followed_id);
+    $query->execute();
+}
+else{
+    $query = $mysqli->prepare("DELETE FROM follow_relations where follower_id=? and followed_id=?");
+    $query->bind_param("ii", $follower_id, $followed_id);
+    $query->execute();
+}
 
 
 ?>
