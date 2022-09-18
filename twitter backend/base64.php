0d009f72096de2aea7484de15023aeeb51a08296
Mohammad Kassem <mohammaddkassemm@gmail.com>
2022-09-18 11:29:44 +0300

    fixing space bug

--- twitter backend/base64.php
+++ twitter backend/base64.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 include("connection.php");
 
-$tweets_tweet_id  = $_POST["tweets_tweet_id "];
+$tweets_tweet_id  = $_POST["tweets_tweet_id"];
 $tweets_picture_url = $_POST["tweets_picture_url"];
 
 $query = $mysqli->prepare("INSERT INTO tweets_picture(tweets_tweet_id ,tweets_picture_url) VALUE(?,?)");
