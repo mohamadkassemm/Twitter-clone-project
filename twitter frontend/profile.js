1e4a4494b5c0f68a7f85a715b52788ae3e2addaf
noormesslmani <noor.messlmani@gmail.com>
2022-09-18 04:56:12 +0300

    displayed tweets in user profile

--- twitter frontend/profile.js
+++ twitter frontend/profile.js
@@ -1,23 +1,28 @@
 const search=document.getElementById('search')
+const searchIcon= document.getElementById('search-icon')
 const home=document.getElementById('home')
 const twitter=document.getElementById('twitter')
 const profile=document.getElementById('profile')
 const themes=document.getElementById('themes')
 const more=document.getElementById('more')
+const tweetContents=document.getElementById('tweet-contents')
+const submitTweet=document.getElementById('submit-tweet')
 const body=document.getElementById('body')
-const modal = document.getElementById("myModal")
+const modal = document.getElementById("myModal");
 const logOut = document.getElementById("logout-btn")
 const cancel = document.getElementById("cancel-btn")
-home.addEventListener('click',()=>{
-    window.location.replace('./home.html')
-})
-twitter.addEventListener('click',()=>{
-    window.location.replace('home.html')
+const searchOutput = document.getElementById('search-output')
+const tweets = document.getElementById('tweets')
+
+
+profile.addEventListener('click',()=>{
+    window.location.replace('profile.html')
 })
 themes.addEventListener('click',()=>{
     body.classList.toggle("black-back")
+    
 })
-
+console.log(window.localStorage)
 more.onclick = function() {
     modal.style.display = "block";
 }
@@ -30,3 +35,33 @@ window.onclick = function(event) {
       modal.style.display = "none";
     }
 }
+
+searchIcon.onclick= ()=>{
+    let userName=search.value
+    let url='http://localhost/twitter-testing/searchusers.php/?users_name='+userName
+    console.log(url)
+    fetch(url)
+    .then(Response => Response.json())
+    .then(data => {
+        localStorage.setItem('data',JSON.stringify(data))
+        window.location.replace('search.html')
+    })  
+}
+console.log(localStorage.id)
+
+let tweetsInfo={
+    method: 'POST',
+    body: new URLSearchParams({users_id:localStorage.id})   
+}
+fetch("http://localhost/twitter-testing/gettweets.php",tweetsInfo)
+.then(Response => Response.json())
+.then(data => {
+    for (let i=0; i< data.length;i++){
+        let para= document.createElement('p')
+        para.setAttribute('class','tweet-contents')
+        console.log(data[i].tweets_content)
+        para.innerText= `${data[i].tweets_content} \n created at ${data[i].tweets_created_at} `
+        tweets.appendChild(para)
+        tweets.appendChild(document.createElement("hr"))
+    }
+})
